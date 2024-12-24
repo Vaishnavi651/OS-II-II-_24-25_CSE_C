3f146889b9bf8f0290079d3a3c07c2f1103b77ca
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2024-12-24 22:37:50 +0530

    Update FCFS.c

--- 23WH1A05G4/FCFS.c
+++ 23WH1A05G4/FCFS.c
@@ -1,44 +1,76 @@
 #include<stdio.h>
-void waitingtime(int p[],int n,int b[],int w[]){
-        w[0]=0;
-        for(int i=1;i<n;i++){
-                w[i]=b[i-1]+w[i-1];
+
+//sort processes based on burst time
+void sortProcesses(int p[], int n, int b[]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (b[j] > b[j + 1]) {
+                // Swap burst times
+                int temp = b[j];
+                b[j] = b[j + 1];
+                b[j + 1] = temp;
+
+                // Swap process IDs
+                int tempP = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = tempP;
+            }
         }
+    }
 }
 
-void turnaroundtime(int p[],int n,int tat[],int b[],int w[]){
-        for(int i=0;i<n;i++){
-                tat[i]=b[i]+w[i];
-        }
+void waitingtime(int p[], int n, int b[], int w[]) {
+    w[0] = 0;
+    for (int i = 1; i < n; i++) {
+        w[i] = b[i - 1] + w[i - 1];
+    }
 }
 
-void findavg(int p[],int n,int b[]){
-        int w[n],tat[n],total_wt=0,total_tat=0;
-        waitingtime(p,n,b,w);
-        turnaroundtime(p,n,tat,b,w);
-        printf("Process\tArrival\tBurst time\tWaiting time\tTurnaroundtime\n");
-        for(int i=0;i<n;i++){
-                total_wt=total_wt+w[i];
-                total_tat=total_tat+tat[i];
-
-                printf(" %d\t ", (i + 1));
-                printf("%d\t",p[i]);
-                printf("%d\t\t ", b[i]);
-                printf("%d\t\t", w[i]);
-                printf("%d\n",tat[i]);
-        }
-
-        int weight=(float)total_wt/(float)n;
-        int turntime=(float)total_tat/(float)n;
-        printf("\nAverage waiting time : %d",weight);
-        printf("\n");
-        printf("Average turnaround time : %d",turntime);
+void turnaroundtime(int p[], int n, int tat[], int b[], int w[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = b[i] + w[i];
+    }
 }
 
-int main(){
-        int p[]={1,2,3};
-        int n=sizeof(p)/sizeof(p[0]);
-        int b[]={6,7,9};
-        findavg(p,n,b);
+void findavg(int p[], int n, int b[]) {
+    int w[n], tat[n], total_wt = 0, total_tat = 0;
 
+    // Sort processes by burst time
+    sortProcesses(p, n, b);
+
+    waitingtime(p, n, b, w);
+    turnaroundtime(p, n, tat, b, w);
+
+    printf("Process\tBurst time\tWaiting time\tTurnaround time\n");
+    for (int i = 0; i < n; i++) {
+        total_wt += w[i];
+        total_tat += tat[i];
+
+        printf(" %d\t\t%d\t\t%d\t\t%d\n", p[i], b[i], w[i], tat[i]);
+    }
+
+    float avg_wt = (float)total_wt / n;
+    float avg_tat = (float)total_tat / n;
+    printf("\nAverage waiting time : %.2f", avg_wt);
+    printf("\nAverage turnaround time : %.2f\n", avg_tat);
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int p[n], b[n];
+
+    // Input process IDs and burst times
+    for (int i = 0; i < n; i++) {
+        p[i] = i + 1; // Process IDs start from 1
+        printf("Enter burst time for process %d: ", p[i]);
+        scanf("%d", &b[i]);
+    }
+
+    findavg(p, n, b);
+
+    return 0;
 }
