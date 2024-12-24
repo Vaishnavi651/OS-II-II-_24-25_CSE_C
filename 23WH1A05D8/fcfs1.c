a3d749e3dbf8cf4942b096f845e37c56d80ff432
DharaniDevi-19 <154337197+DharaniDevi-19@users.noreply.github.com>
2024-12-24 15:56:22 +0530

    Update fcfs.c

--- 23WH1A05D8/fcfs1.c
+++ 23WH1A05D8/fcfs1.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+void tur(int n,int ct[n],int at[n],int tt[n]);
 // Function to swap processes based on their arrival time
 void swap(int n, int p[n], int at[n], int bt[n]);
 
 // Function to calculate completion times based on burst times
 void com(int n, int bt[n], int ct[n]);
+
+void wai(int n,int tt[n],int bt[n],int wt[n]);
 
 int main() {
     int n, i, k;
@@ -13,7 +15,7 @@ int main() {
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int p[n], at[n], bt[n], ct[n];  // Arrays for process ID, arrival time, burst time, and completion time
+    int p[n], at[n], bt[n], ct[n],tt[n],wt[n];  // Arrays for process ID, arrival time, burst time, and completion time
 
     // Initialize process IDs
     for(i = 0; i < n; i++) {
@@ -37,14 +39,27 @@ int main() {
 
     // Calculate completion times
     com(n, bt, ct);
+    
+    tur(n,ct,at,tt);
 
+    wai(n,tt,bt,wt);
     // Output the details (Process ID, Arrival Time, Burst Time, Completion Time)
-    printf("\nProcess ID\tArrival Time\tBurst Time\tCompletion Time\n");
+    int sum,add;
+    printf("\nPID\t\tAT\t\tBT\t\tCT\t\ttt\t\twt\n");
     for(k = 0; k < n; k++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\n", p[k], at[k], bt[k], ct[k]);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[k], at[k], bt[k], ct[k],tt[k],wt[k]);
+    
+    
+        sum = sum +  tt[k];
+        add = add + wt[k];
+    
     }
-
-    return 0;
+         int avg = sum /n;
+         int avg1 = add/n;
+         printf("Average turn around time is : %d\n",avg);
+         printf("Average waiting time is : %d",avg1);
+       
+       return 0;
 }
 
 // Function to swap processes based on arrival time (sorting)
@@ -79,6 +94,27 @@ void com(int n, int bt[n], int ct[n]) {
     for(i = 0; i < n; i++) {
         sum += bt[i];  // Accumulate burst times to get completion times
         ct[i] = sum;   // Set the completion time for process i
+    }
+}
+
+void tur(int n,int ct[n],int at[n],int tt[n])
+{
+    int sub = 0,i;
+    
+    for(i=0;i<n;i++)
+    {
+        sub = ct[i] -at[i];
+        tt[i] = sub;
+        }
+}
+
+void wai(int n,int tt[n],int bt[n],int wt[n])
+{
+    int su = 0,i;
+    
+    for(i=0;i<n;i++){
+        su = tt[i] - bt[i];
+        wt[i] = su;
     }
 }
 
