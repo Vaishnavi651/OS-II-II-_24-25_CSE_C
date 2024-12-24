8745ccbc4886e787a1a6bed0f604646bf1eed293
Tamkinf4 <154491872+Tamkinf4@users.noreply.github.com>
2024-12-24 11:39:05 +0530

    Update fcfs.c

--- 23wh1a05f4/fcfs.c
+++ 23wh1a05f4/fcfs.c
@@ -1,31 +1,66 @@
-#include<stdio.h>
-struct process{
-	int id;
-	int arrivaltime;
-	int bursttime;
-	int waitingtime;
-	int turnaroundtime;
-	};
-void calculatewaitingtime(struct process processes[],int n)
+#include<iostream>
+using namespace std;
+
+
+void findWaitingTime(int processes[], int n, 
+                          int bt[], int wt[])
 {
-processes[0].waitingtime=0;
-for(int i=1;i<n;i++)
-{
-processes[i].waitingtime=processes[i-1].waitingtime+processes[i-1].bursttime;
-}
+    
+    wt[0] = 0;
+
+    // calculating waiting time
+    for (int  i = 1; i < n ; i++ )
+        wt[i] =  bt[i-1] + wt[i-1] ;
 }
 
-void calculateturnaroundtime(struct process processes[], int n)
+
+void findTurnAroundTime( int processes[], int n, 
+                  int bt[], int wt[], int tat[])
 {
-for(int i=0;i<n;i++)
-processes[i].turnaroundtime=processes[i].waitingtime+processes[i].bursttime;
+    
+    for (int  i = 0; i < n ; i++)
+        tat[i] = bt[i] + wt[i];
 }
 
-void calculateaveragetimes(struct process processes[],int n)
+
+void findavgTime( int processes[], int n, int bt[])
 {
-float totalwaitingtime=0,totalturnaroundtime=0;
-for(i=0;i<n;i++)
-totalwaitingtime+=processes[i].waitingtime;
-totalturnarountime+=processes[i].turnaroundtime;
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    
+    findWaitingTime(processes, n, bt, wt);
+
+    
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    
+    cout << "Processes  "<< " Burst time  "
+         << " Waiting time  " << " Turn around time\n";
+
+    
+    for (int  i=0; i<n; i++)
+    {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        cout << "   " << i+1 << "\t\t" << bt[i] <<"\t    "
+            << wt[i] <<"\t\t  " << tat[i] <<endl;
+    }
+
+    cout << "Average waiting time = " 
+         << (float)total_wt / (float)n;
+    cout << "\nAverage turn around time = " 
+         << (float)total_tat / (float)n;
 }
-printf("Average waiting time=%.2f\n",
+
+int main()
+{
+    
+    int processes[] = { 1, 2, 3};
+    int n = sizeof processes / sizeof processes[0];
+
+    
+    int  burst_time[] = {10, 5, 8};
+
+    findavgTime(processes, n,  burst_time);
+    return 0;
+}
