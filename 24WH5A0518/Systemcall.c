2ca864016ace4096948845106f7280f11d81adf9
SadiyaFatima21 <24wh5a0518@bvrithyderabad.edu.in>
2025-01-20 15:34:47 +0530

    Update Systemcall

--- 24WH5A0518/Systemcall.c
+++ 24WH5A0518/Systemcall.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
 int main() {
-    pid_t pid = fork(); 
-    int status;
+    pid_t pid = fork();
 
     if (pid < 0) {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
+        perror("Fork Failed");
+        return 1;
     }
-
-    if (pid == 0) {
-        execl("/bin/ls", "ls", NULL);
-        perror("execl failed");
-        exit(EXIT_FAILURE);
-    } else {
-        pid_t waitResult = waitpid(pid, &status, 0);
-        if (waitResult == -1) {
-            perror("waitpid failed");
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(status)) {
-            printf("Child Process (PID = %d) terminated with exit status: %d\n", pid, WEXITSTATUS(status));
-        }
+    else if (pid == 0) {
+        printf("Child PID: %d\n", getpid());
+        execlp("cal", "cal", NULL);
+        perror("exec Failed");
+        exit(1);
     }
-
+    else {
+        printf("Parent PID: %d\n", getpid());
+        int status;
+        wait(&status);
+        printf("Child process finished with status %d\n", WEXITSTATUS(status));
+    }
     return 0;
 }
