d7c875abcfe24996a107a62045c98ed5e2d4e4cc
Akshaya1859 <154488941+Akshaya1859@users.noreply.github.com>
2025-01-21 10:50:07 +0530

    Add files via upload

---
+++ 23WH1A05E9/system.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(1);
+    }
+    if (pid == 0) {
+        char *args[] = {"./fc", NULL};  
+        execvp(args[0], args);
+        perror("exec failed");  
+        exit(1);
+    } else {
+        printf("Parent Process: PID = %d, Child PID = %d\n", getpid(), pid);
+        int status;
+        wait(&status);  
+        printf("Parent Process: Child has finished with status %d\n", status);
+        exit(0);
+    }
+    return 0;
+}
+
+
