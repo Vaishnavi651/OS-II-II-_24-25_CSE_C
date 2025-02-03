cc0593453465ad32cfbf3af69ccbdfe4b9f29a11
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2025-02-03 14:02:16 +0530

    Update SJF.c

--- 23WH1A05G4/SJF.c
+++ 23WH1A05G4/SJF.c
@@ -1,104 +1,56 @@
+
 #include <stdio.h>
-
-void sortByBurstTime(int p[], int a[], int b[], int n);
-void calculateCompletionTime(int p[], int a[], int b[], int completionTime[], int n);
-void calculateTurnaroundTime(int a[], int completionTime[], int turnaroundTime[], int n);
-void calculateWaitingTime(int turnaroundTime[], int b[], int waitingTime[], int n);
-
-int main() {
-    int n, i;
-
-    printf("Enter the number of processes: ");
+int main()
+{
+    // Matrix for storing Process Id, Burst
+    // Time, Average Waiting Time & Average
+    // Turn Around Time.
+    int A[100][4];
+    int i, j, n, total = 0, index, temp;
+    float avg_wt, avg_tat;
+    printf("Enter number of process: ");
     scanf("%d", &n);
-
-    int p[n], a[n], b[n];
-    int completionTime[n], turnaroundTime[n], waitingTime[n];
-
-    printf("Enter the process IDs: ");
+    printf("Enter Burst Time:\n");
+    // User Input Burst Time and alloting Process Id.
     for (i = 0; i < n; i++) {
-        scanf("%d", &p[i]);
+        printf("P%d: ", i + 1);
+        scanf("%d", &A[i][1]);
+        A[i][0] = i + 1;
     }
-
+    // Sorting process according to their Burst Time.
     for (i = 0; i < n; i++) {
-        printf("Enter the arrival time of process %d: ", p[i]);
-        scanf("%d", &a[i]);
-        printf("Enter the burst time of process %d: ", p[i]);
-        scanf("%d", &b[i]);
+        index = i;
+        for (j = i + 1; j < n; j++)
+            if (A[j][1] < A[index][1])
+                index = j;
+        temp = A[i][1];
+        A[i][1] = A[index][1];
+        A[index][1] = temp;
+ 
+        temp = A[i][0];
+        A[i][0] = A[index][0];
+        A[index][0] = temp;
     }
-
-    sortByBurstTime(p, a, b, n);
-
-    calculateCompletionTime(p, a, b, completionTime, n);
-    calculateTurnaroundTime(a, completionTime, turnaroundTime, n);
-    calculateWaitingTime(turnaroundTime, b, waitingTime, n);
-
-    printf("\nProcessID ArrivalTime BurstTime CompletionTime TurnaroundTime WaitingTime\n");
+    A[0][2] = 0;
+    // Calculation of Waiting Times
+    for (i = 1; i < n; i++) {
+        A[i][2] = 0;
+        for (j = 0; j < i; j++)
+            A[i][2] += A[j][1];
+        total += A[i][2];
+    }
+    avg_wt = (float)total / n;
+    total = 0;
+    printf("P     BT     WT     TAT\n");
+    // Calculation of Turn Around Time and printing the
+    // data.
     for (i = 0; i < n; i++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], a[i], b[i], completionTime[i], turnaroundTime[i], waitingTime[i]);
+        A[i][3] = A[i][1] + A[i][2];
+        total += A[i][3];
+        printf("P%d     %d     %d      %d\n", A[i][0],
+               A[i][1], A[i][2], A[i][3]);
     }
-
-    float totalTurnaroundTime = 0, totalWaitingTime = 0;
-    for (i = 0; i < n; i++) {
-        totalTurnaroundTime += turnaroundTime[i];
-        totalWaitingTime += waitingTime[i];
-    }
-
-    printf("\nAverage Turnaround Time: %.2f\n", totalTurnaroundTime / n);
-    printf("Average Waiting Time: %.2f\n", totalWaitingTime / n);
-
-    return 0;
+    avg_tat = (float)total / n;
+    printf("Average Waiting Time= %f", avg_wt);
+    printf("\nAverage Turnaround Time= %f", avg_tat);
 }
-
-void sortByBurstTime(int p[], int a[], int b[], int n) {
-    int i, j, temp;
-    // Sorting processes based on burst time (ascending order)
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (b[j] > b[j+1] || b[j]==b[j+1] && a[j]>a[j+1]) {
-                // Swap burst times
-                temp = b[j];
-                b[j] = b[j+1];
-                b[j+1] = temp;
-
-                // Swap arrival times
-                temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
-
-                // Swap process IDs
-                temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
-            }
-        }
-    }
-}
-
-void calculateCompletionTime(int p[], int a[], int b[], int completionTime[], int n) {
-    int currentTime = 0;
-    for (int i = 0; i < n; i++) {
-        // If the current time is less than the arrival time, wait until the process arrives
-        if (currentTime < a[i]) {
-            currentTime = a[i];
-        }
-        // Add the burst time to the current time to find the completion time
-        currentTime += b[i];
-        completionTime[i] = currentTime;
-    }
-}
-
-void calculateTurnaroundTime(int a[], int completionTime[], int turnaroundTime[], int n) {
-    for (int i = 0; i < n; i++) {
-        turnaroundTime[i] = completionTime[i] - a[i]; // Turnaround time = Completion time - Arrival time
-    }
-}
-
-void calculateWaitingTime(int turnaroundTime[], int b[], int waitingTime[], int n) {
-    for (int i = 0; i < n; i++) {
-        waitingTime[i] = turnaroundTime[i] - b[i];
-        if(waitingTime[i]<0){
-            waitingTime[i]=0;
-        }// Waiting time = Turnaround time - Burst time
-    }
-}
-
