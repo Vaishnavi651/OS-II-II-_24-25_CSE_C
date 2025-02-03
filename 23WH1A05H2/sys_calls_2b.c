5868009358e19ffe7a27998c1a6e97369244d5d5
Varshini0703 <153826159+Varshini0703@users.noreply.github.com>
2025-02-03 15:35:02 +0530

    Update sys_calls_2b.c

--- 23WH1A05H2/sys_calls_2b.c
+++ 23WH1A05H2/sys_calls_2b.c
@@ -1,28 +1,48 @@
-#include<stdio.h>
-#include<dirent.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<sys/stat.h>
-#include<unistd.h>
+#include <stdio.h>
 #include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<dirent.h>
 
-int main(){
-        int fd;
-        char buffer[100];
-        fd = open("test.txt",O_WRONLY|O_CREAT,0644);
-        if(fd==-1){
-                printf("Error! failed in creating a file");
-                return 1;
-        }
-        write(fd,"Hello, I am Varshini, nice to meet you!", 39);
-        close(fd);
-        fd = open("test.txt",O_RDONLY);
-        if(fd==-1){
-                perror("Error has occured");
-        }
-        read(fd,buffer,sizeof(buffer));
-        printf("Reading from file...%s",buffer);
-        close(fd);
-
-}
-
+int main() {
+    int fd;
+    char buff[100];
+    fd = open("sample.txt",O_WRONLY | O_CREAT, 0644);
+    if(fd==-1){
+        perror("Error has occured!");
+        return 1;
+    }
+    write(fd,"Hello\n",7);
+    close(fd);
+    fd = open("sample.txt", O_RDONLY);
+    if(fd==-1){
+        perror("Error ochindi ra samba");
+    }
+    read(fd,buff,sizeof(buff));
+    printf("Read from file: %s",buff);
+    close(fd);
+    off_t offset = lseek(fd,0, SEEK_SET);
+    if(offset == (off_t)-1){
+        perror("Error ra orey");
+        return 1;
+    }
+    struct stat fs;
+    if(lstat("sample.txt",&fs) == -1){
+        perror("Error");
+    }
+    printf("File size: %lld bytes\n",(long long)fs.st_size);
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(".");
+    if(dir==NULL){
+        perror("Error in opening");
+        return 1; 
+    }
+    printf("Directory contents: \n");
+    while((entry =  readdir(dir)) != NULL){
+        printf("%s\n",entry->d_name);
+    }
+    closedir(dir);
+    return 0;
+}   
