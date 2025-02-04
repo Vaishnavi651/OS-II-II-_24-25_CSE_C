05676c8bdbd914f6d46c55f250976e98743b7496
likhithasree15 <153829741+likhithasree15@users.noreply.github.com>
2025-02-04 10:56:59 +0530

    Add files via upload

---
+++ 23WH1A05E5/System calls2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<dirent.h>
+
+int main()
+{
+char buffer[100];
+ssize_t bytes;
+int fd = open("hii.txt",O_WRONLY|O_CREAT|O_TRUNC,0644);
+if(fd < 0)
+{
+printf("open failed");
+return 1;
+}
+write(fd,"hello world ,this is me\n",24);
+close(fd);
+fd = open("hii.txt",O_RDONLY);
+bytes = read(fd,buffer,sizeof(buffer)-1);
+buffer[bytes] = '\0';
+printf("The contents of the file : %s\n",buffer);
+close(fd);
+fd = open("hii.txt",O_WRONLY);
+lseek(fd,0,SEEK_END);
+write(fd,"This is my first program\n",25);
+close(fd);
+struct stat st;
+stat("hii.txt",&st);
+printf("File size : %lld bytes\n",(long long)st.st_size);
+DIR *dir = opendir(".");
+struct dirent *entry;
+printf("Directory contents : \n");
+while((entry = readdir(dir))) printf("%s\n",entry->d_name);
+closedir(dir);
+return 0;
+}
