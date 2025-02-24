5e5e0b520a59acb4670be058dc94fc8739ef8dde
siddi-Harshini <154400547+siddi-Harshini@users.noreply.github.com>
2025-02-24 22:54:35 +0530

    Update FCFS.cpp

--- 23WH1A05C9_OS/CPU_Scheduling_algorithm/FCFS.cpp
+++ 23WH1A05C9_OS/CPU_Scheduling_algorithm/FCFS.cpp
@@ -1,44 +1,8 @@
 #include <stdio.h>
-
-void findWaitingTime(int processes[], int n, int bt[], int wt[], int at[]) {
-    int service_time[n];
-    service_time[0] = at[0];
-    wt[0] = 0;
-
-    for (int i = 1; i < n; i++) {
-        service_time[i] = service_time[i - 1] + bt[i - 1];
-        wt[i] = service_time[i] - at[i];
-
-        if (wt[i] < 0) {
-            wt[i] = 0;
-        }
-    }
-}
-
-void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
-    for (int i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
-    }
-}
-
-void findAvgTime(int processes[], int n, int bt[], int at[]) {
-    int wt[n], tat[n], total_wt = 0, total_tat = 0;
-    findWaitingTime(processes, n, bt, wt, at);
-    findTurnAroundTime(processes, n, bt, wt, tat);
-
-    printf("Processes\tBurst Time\tWaiting Time\tTurn-Around Time\n");
-    for (int i = 0; i < n; i++) {
-        total_wt += wt[i];
-        total_tat += tat[i];
-        printf("%d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
-    }
-
-    printf("Average waiting time = %.2f\n", (float)total_wt / (float)n);
-    printf("Average turn-around time = %.2f\n", (float)total_tat / (float)n);
-}
-
 void fcfsScheduling(int processes[], int n, int bt[], int at[]) {
     int temp;
+    int ct[n], tat[n], wt[n];
+    int total_tat = 0, total_wt = 0;
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (at[i] > at[j]) {
@@ -56,14 +20,28 @@ void fcfsScheduling(int processes[], int n, int bt[], int at[]) {
             }
         }
     }
-    findAvgTime(processes, n, bt, at);
-}
+    printf("\nProcesses\tBurst Time\tArrival Time\tCompletion Time\tTurn-Around Time\tWaiting Time\n");
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            ct[i] = at[i] + bt[i];
+        } else {
+            ct[i] = (ct[i - 1] > at[i] ? ct[i - 1] : at[i]) + bt[i];
+        }
+        
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
 
+        total_tat += tat[i];
+        total_wt += wt[i];
+        printf("P%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], at[i], ct[i], tat[i], wt[i]);
+    }
+    printf("\nAverage Turnaround Time = %.2f\n", (float)total_tat / n);
+    printf("Average Waiting Time = %.2f\n", (float)total_wt / n);
+}
 int main() {
     int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-
     int processes[n], burst_time[n], arrival_time[n];
     for (int i = 0; i < n; i++) {
         processes[i] = i + 1;
@@ -72,9 +50,6 @@ int main() {
         printf("Enter arrival time for process %d: ", i + 1);
         scanf("%d", &arrival_time[i]);
     }
-
     fcfsScheduling(processes, n, burst_time, arrival_time);
-
     return 0;
 }
-
