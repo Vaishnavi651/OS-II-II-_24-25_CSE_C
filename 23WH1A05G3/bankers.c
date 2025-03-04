dc054546aeddf8b0b13692d4ab34d82a6addc04f
Adifa saniya <153834311+adifa-7@users.noreply.github.com>
2025-03-04 11:50:30 +0530

    Add files via upload

---
+++ 23WH1A05G3/bankers.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void calculateNeed(int n, int m, int max[n][m], int allocation[n][m], int need[n][m])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+	{
+            need[i][j] = max[i][j] - allocation[i][j];
+	}
+    }
+}
+
+bool isSafe(int n, int m, int available[m], int max[n][m], int allocation[n][m], int need[n][m])
+{
+	int work[m];
+	bool finish[n];
+
+	for(int i=0; i<m; i++)
+	{
+		work[i] = available[i];
+	}
+	for(int i=0; i<n; i++)
+	{
+		finish[i] = false;
+	}
+
+	int safe[n];
+	int count = 0;
+	while(count < n)
+	{
+		bool found = false;
+		for(int i=0; i<n; i++)
+		{
+			if(!finish[i])
+			{
+				bool canAllocate = true;
+				for(int j=0; j<m; j++)
+				{
+					if(need[i][j] > work[j])
+					{
+						canAllocate = false;
+						break;
+					}
+				}
+				if(canAllocate)
+				{
+					for(int j=0; j<m; j++)
+					{
+						work[j] += allocation[i][j];
+					}
+					safe[count++] = i;
+					finish[i] = true;
+					found = true;
+				}
+			}
+		}
+		if(!found)
+		{
+			return false;
+		}
+
+	}
+    printf("Safe Sequence: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("P%d ", safe[i]);
+    }
+    printf("\n");
+    return true;
+}
+ 
+int main() 
+{
+    int n, m;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
+
+    int available[m];
+    int max[n][m], allocation[n][m], need[n][m];
+
+    printf("Enter the available resources:\n");
+    for (int i = 0; i < m; i++) 
+    {	
+	printf("Resource %d: ", i + 1);
+        scanf("%d", &available[i]);
+    }
+
+    printf("Enter the maximum resources for each process:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("For Process P%d:\n", i + 1);
+        for (int j = 0; j < m; j++)
+       	{
+            printf("Max resource %d: ", j + 1);
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    printf("Enter the allocation for each process:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("For Process P%d:\n", i + 1);
+        for (int j = 0; j < m; j++) 
+	{
+            printf("Allocated resource %d: ", j + 1);
+            scanf("%d", &allocation[i][j]);
+        }
+    }
+	
+    calculateNeed(n, m, max, allocation, need);
+
+    if (isSafe(n, m, available, max, allocation, need))
+    {
+        printf("System is in a safe state.\n");
+    }
+    else
+    {
+        printf("System is in an unsafe state!\n");
+    }
+
+    return 0;
+}
+
