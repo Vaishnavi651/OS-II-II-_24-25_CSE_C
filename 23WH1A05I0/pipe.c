7e85911fbb7644726168515ed70811b819213bd0
gayathri817 <23wh1a05i0@bvrithyderabad.edu.in>
2025-03-10 16:09:40 +0530

    Create pipe.c

---
+++ 23WH1A05I0/pipe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+int main() {
+    int fd[2];
+    char buffer[100];
+    if (pipe(fd) == -1) {
+        perror("pipe failed");
+        return(1);
+    }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        return(1);
+    }
+    else if (pid == 0) {
+        close(fd[1]);
+        read(fd[0], buffer, sizeof(buffer));
+        printf("child received: %s\n", buffer);
+        close(fd[0]);
+    }
+    else {
+        close(fd[0]);
+        char *msg = "Hello from parent process!";
+        write(fd[1], msg, strlen(msg)+1);
+        close(fd[1]);
+}
+return 0;
+}
+
