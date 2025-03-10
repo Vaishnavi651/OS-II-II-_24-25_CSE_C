e1b5b40de3fe1757debce9a6c858ead998b92a24
Haripriya-23wh1a05g9 <153830065+Haripriya-23wh1a05g9@users.noreply.github.com>
2025-03-10 16:02:25 +0530

    Create pipes.c

---
+++ 23WH1A05G9/pipes.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+int main(){
+	int fd[2];
+	char buffer[100];
+	if(pipe(fd)==-1){
+		perror("pipe failed");
+		return 1;
+	}
+	pid_t pid=fork();
+	if(pid<0){
+		perror("fork failed");
+		return 1;
+	}
+	else if(pid ==0){
+		close(fd[1]);
+		read(fd[0],buffer,sizeof(buffer));
+		printf("child retrived:%s\n",buffer);
+		close(fd[0]);
+	}
+	else{
+		close(fd[0]);
+		char msg[100] = "heelo from parent";
+		write(fd[1],msg,strlen(msg)+1);
+		close(fd[1]);
+	}
+	return 0;
+}
