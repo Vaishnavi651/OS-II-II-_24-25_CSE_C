68d485a3a1734e38d9aba8c1f8e5e82d38afa0bd
SalenkiShivani23 <24wh5a0516@bvrithyderabad.edu.in>
2025-03-10 16:00:45 +0530

    Create Producer Consumer.c

---
+++ 24WH5A0516/Producer Consumer.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int mutex = 1;     
+int full = 0;      
+int empty;         
+int x = 0;         
+
+void producer()
+{    
+    --mutex;
+    ++full;   
+    --empty;
+    x++;
+    printf("\nProducer produces item %d", x);
+    printf("\nBuffer Status: %d full, %d empty", full, empty);
+    ++mutex;
+}
+
+void consumer()
+{   
+    --mutex;   
+    --full;
+    ++empty;
+    printf("\nConsumer consumes item %d", x);
+    printf("\nBuffer Status: %d full, %d empty", full, empty);
+    x--;
+    ++mutex;
+}
+
+int main()
+{
+    int n, i;
+    
+    
+    printf("Enter the buffer size: ");
+    scanf("%d", &empty);
+
+    printf("\n1. Press 1 for Producer"
+           "\n2. Press 2 for Consumer"
+           "\n3. Press 3 for Exit");
+    
+    for (i = 1; i > 0; i++) {
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &n);
+        switch (n) {
+        case 1:                           
+            if ((mutex == 1) && (empty != 0)) {
+                producer();
+            }
+            else {
+                printf("\nBuffer is full! Cannot produce more.");
+                printf("\nBuffer Status: %d full, %d empty", full, empty);
+            }
+            break;
+        case 2:
+            if ((mutex == 1) && (full != 0)) {
+                consumer();
+            }
+            else {
+                printf("\nBuffer is empty! Cannot consume.");
+                printf("\nBuffer Status: %d full, %d empty", full, empty);
+            }
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("\n Not valid ! Try again.");
+        }
+    }
+}
