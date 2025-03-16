3e3097e948782c5e48ee1dc9023b8ad52b8da7c1
Ch Harika Reddy <154301279+Harika-07@users.noreply.github.com>
2025-03-16 17:32:42 +0530

    Create producer-consumer.c

---
+++ 23WH1A05H0/producer-consumer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int mutex = 1;
+int full = 0;
+int empty = 10, x = 0;
+
+void wait(int *S) {
+    while (*S <= 0);
+    (*S)--;
+}
+
+void signal(int *S) {
+    (*S)++;
+}
+
+void producer() {
+    wait(&empty);
+    wait(&mutex);
+
+    x++;
+    printf("\nProducer produces item %d", x);
+
+    signal(&mutex);
+    signal(&full);
+}
+
+void consumer() {
+    do {
+        wait(&full);
+        wait(&mutex);
+
+        printf("\nConsumer consumes item %d", x);
+        x--;
+
+        signal(&mutex);
+        signal(&empty);
+
+        sleep(3);
+    } while (x > 0);
+}
+
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        producer();
+        sleep(1);
+        consumer();
+    }
+
+    return 0;
+}
