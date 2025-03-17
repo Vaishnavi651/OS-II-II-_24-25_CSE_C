9a54fd3d7adc0170b7651d405971783a060b7125
Akhilai6 <154301436+Akhilai6@users.noreply.github.com>
2025-03-17 16:05:02 +0530

    Create fifo.c

---
+++ 23WH1A05I6/fifo.c
@@ -0,0 +1,59 @@
+writer
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+    char msg[100];
+
+    printf("Enter a message: ");
+    fgets(msg, sizeof(msg), stdin);
+
+    fd = open("myfifo", O_WRONLY);
+    if (fd == -1) {
+        perror("Error opening FIFO");
+        exit(1);
+    }
+
+    write(fd, msg, strlen(msg) + 1);
+    printf("Writer: Message sent to FIFO\n");
+
+    close(fd);
+    return 0;
+}
+
+
+
+reader
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+    char buffer[100];
+
+    fd = open("myfifo", O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening FIFO");
+        exit(1);
+    }
+
+    read(fd, buffer, sizeof(buffer));
+    printf("Reader: Received message: %s\n", buffer);
+
+    close(fd);
+    return 0;
+}
+
+
+	
