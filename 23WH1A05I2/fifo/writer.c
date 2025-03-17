5274876d929967be08f7fdb095fb5f9651946a14
SahasraI2 <153825867+SahasraI2@users.noreply.github.com>
2025-03-17 14:45:31 +0530

    Create writer.c

---
+++ 23WH1A05I2/fifo/writer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+    char msg[100];
+
+    printf("Enter a message: ");
+    fgets(msg, sizeof(msg), stdin);
+
+    fd = open("myfifo", O_WRONLY);
+    if (fd == -1) {
+        perror("Error opening FIFO");
+        exit(1);
+    }
+
+    write(fd, msg, strlen(msg) + 1);
+    printf("Writer: Message sent to FIFO\n");
+
+    close(fd);
+    return 0;
+}
