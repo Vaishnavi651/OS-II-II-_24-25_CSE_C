eba4c195dae5317334595f3e72e4d52181c77410
SahasraI2 <153825867+SahasraI2@users.noreply.github.com>
2025-03-17 15:51:50 +0530

    Create read.c

---
+++ 23WH1A05I2/sharedmemory/read.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = ftok("file", 65);  
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);  
+    char *str = (char *)shmat(shmid, NULL, 0);  
+    
+    printf("Received from shared memory at address %p: %s", str, str);  
+    shmdt(str);  
+    shmctl(shmid, IPC_RMID, NULL);  
+    return 0;
+}
