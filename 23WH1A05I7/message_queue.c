6c12b32ff9294a4ffb29f00ef779296b8df874f3
keertiI7 <153827036+keertiI7@users.noreply.github.com>
2025-03-17 16:05:33 +0530

    Create message_queue.c

---
+++ 23WH1A05I7/message_queue.c
@@ -0,0 +1,58 @@
+sender
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct msg_buffer {
+    long msg_type;
+    char msg_text[100];
+};
+
+int main() {
+    key_t key = ftok("file", 65);  
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    struct msg_buffer message;
+
+    message.msg_type = 1;
+    printf("Enter message: ");
+    fgets(message.msg_text, sizeof(message.msg_text), stdin);
+
+    msgsnd(msgid, &message, sizeof(message.msg_text), 0);
+    printf("Message sent!\n");
+
+    return 0;
+}
+
+
+
+
+receiver
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct msg_buffer {
+    long msg_type;
+    char msg_text[100];
+};
+
+int main() {
+    key_t key = ftok("file", 65);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    struct msg_buffer message;
+
+    msgrcv(msgid, &message, sizeof(message.msg_text), 1, 0);
+    printf("Received: %s", message.msg_text);
+
+    msgctl(msgid, IPC_RMID, NULL); // Remove message queue
+    return 0;
+}
+
+
