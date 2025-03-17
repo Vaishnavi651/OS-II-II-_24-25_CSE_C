3ce179532cafd52d5ef0e6b8eda4bb2a456ee393
Tejasreei1 <154443497+Tejasreei1@users.noreply.github.com>
2025-03-17 16:06:03 +0530

    Create reader.c

---
+++ 23WH1A05I1/SHARED MEMORY/reader.c
@@ -0,0 +1,18 @@
+reader
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = ftok("file", 65);  // Generate unique key
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);  // Get shared memory
+    char *str = (char *)shmat(shmid, NULL, 0);  // Attach memory at OS-chosen address
+
+    printf("Received from shared memory at address %p: %s", str, str);  // Print message
+
+    shmdt(str);  // Detach from memory
+    shmctl(shmid, IPC_RMID, NULL);  // Remove shared memory
+    return 0;
+}
