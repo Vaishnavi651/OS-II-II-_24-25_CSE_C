30b032c4b70c37bb0a4120c1ed2f044737b28ea4
gayathri817 <23wh1a05i0@bvrithyderabad.edu.in>
2025-03-17 15:39:32 +0530

    Create IPC read.c

---
+++ 23WH1A05I0/IPC read.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+    char buffer[100];
+
+    fd = open("myfifo", O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening FIFO");
+        exit(1);
+    }
+
+    read(fd, buffer, sizeof(buffer));
+    printf("Reader: Received message: %s\n", buffer);
+
+    close(fd);
+
+    return 0;
+}
