94afcdf701c6a3608276d62c4dd37d81a2ed48a8
Akhilai6 <154301436+Akhilai6@users.noreply.github.com>
2025-03-17 16:09:52 +0530

    Create sharedmemory.c

---
+++ 23WH1A05I6/sharedmemory.c
@@ -0,0 +1,44 @@
+
+writer
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = ftok("file", 65);  
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT); 
+    char *str = (char *)shmat(shmid, NULL, 0);  
+
+    printf("Enter message: ");
+    fgets(str, 1024, stdin);  
+
+    printf("Message written to shared memory at address: %p\n", str);
+
+    shmdt(str);  
+    return 0;
+}
+
+
+reader
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = ftok("file", 65);  
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);  
+    char *str = (char *)shmat(shmid, NULL, 0);  
+
+    printf("Received from shared memory at address %p: %s", str, str);  
+
+    shmdt(str);  
+    shmctl(shmid, IPC_RMID, NULL);  
+    return 0;
+}
+
