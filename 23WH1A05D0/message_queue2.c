4e3bd5c3848b4c8e541f648abbcd04ef8fd030b0
Bhanu-Sri-Rathod <154400326+Bhanu-Sri-Rathod@users.noreply.github.com>
2025-03-18 10:35:33 +0530

    Update message_queue2.c

--- 23WH1A05D0/message_queue2.c
+++ 23WH1A05D0/message_queue2.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-
-// Structure for message queue
 struct msg_buffer {
     long msg_type;
     char msg_text[100];
@@ -13,27 +11,17 @@ int main() {
     key_t key;
     int msgid;
     struct msg_buffer message;
-
-    // Generate unique key
     key = ftok("progfile", 65);
-
-    // Get message queue
     msgid = msgget(key, 0666 | IPC_CREAT);
     if (msgid == -1) {
         perror("msgget failed");
         exit(1);
     }
-
-    // Receive message
     if (msgrcv(msgid, &message, sizeof(message.msg_text), 1, 0) == -1) {
         perror("msgrcv failed");
         exit(1);
     }
-
     printf("Received message: %s\n", message.msg_text);
-
-    // Destroy message queue
     msgctl(msgid, IPC_RMID, NULL);
-
     return 0;
 }
