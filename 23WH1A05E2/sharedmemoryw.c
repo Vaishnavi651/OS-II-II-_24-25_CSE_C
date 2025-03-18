2b63defe93eede409d8c20d9fc9117a7bdf25c54
Vaishnavi651 <154791144+Vaishnavi651@users.noreply.github.com>
2025-03-18 11:46:41 +0530

    Add files via upload

---
+++ 23WH1A05E2/sharedmemoryw.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = ftok("file", 65);  
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT); 
+    char *str = (char *)shmat(shmid, NULL, 0);  
+
+    printf("Enter message: ");
+    fgets(str, 1024, stdin); 
+
+    printf("Message written to shared memory at address: %p\n", str);
+
+    shmdt(str);  
+    return 0;
+}
+
