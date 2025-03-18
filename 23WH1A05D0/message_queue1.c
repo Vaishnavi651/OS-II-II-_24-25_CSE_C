520c322eb75442c49a8fa30d04104646cc1e8b4a
Bhanu-Sri-Rathod <154400326+Bhanu-Sri-Rathod@users.noreply.github.com>
2025-03-18 10:36:20 +0530

    Update message_queue1.c

--- 23WH1A05D0/message_queue1.c
+++ 23WH1A05D0/message_queue1.c
@@ -4,7 +4,6 @@
 #include <sys/msg.h>
 #include <string.h>
 
-// Structure for message queue
 struct msg_buffer {
     long msg_type;
     char msg_text[100];
@@ -15,10 +14,8 @@ int main() {
     int msgid;
     struct msg_buffer message;
 
-    // Generate unique key
     key = ftok("progfile", 65);
 
-    // Create message queue and return identifier
     msgid = msgget(key, 0666 | IPC_CREAT);
     if (msgid == -1) {
         perror("msgget failed");
@@ -30,7 +27,6 @@ int main() {
     fgets(message.msg_text, sizeof(message.msg_text), stdin);
     message.msg_text[strcspn(message.msg_text, "\n")] = 0; // Remove newline
 
-    // Send message
     if (msgsnd(msgid, &message, sizeof(message.msg_text), 0) == -1) {
         perror("msgsnd failed");
         exit(1);
