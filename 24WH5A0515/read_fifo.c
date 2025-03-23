3d24f80304c43cfa7bd5c3af7d0ea0b89fa0fa57
24wh5a0515 <24wh5a0515@bvrithyderabad.edu.in>
2025-03-23 23:58:17 +0530

    Create read_fifo.c

---
+++ 24WH5A0515/read_fifo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include<string.h>
+int main() {
+    int fd;
+    char buffer[100];
+
+    
+    mkfifo("myfifo", 0666);
+
+    while (1) {
+       
+        fd = open("myfifo", O_RDONLY);
+        read(fd, buffer, sizeof(buffer));
+
+        printf("Received: %s", buffer);
+        close(fd);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            break;
+        }
+    }
