c2c88ad4739e35d0aa3a29bb88ea10350f6f9fdf
Akanksha <154399251+akanksha05d1@users.noreply.github.com>
2025-04-16 04:30:13 +0530

    Update worst_fit.c

--- 23WH1A05D1/worst_fit.c
+++ 23WH1A05D1/worst_fit.c
@@ -39,9 +39,21 @@
   printf("%d\t\t%d\t\t%d\n", i + 1, process_size[i],
  allocation[i] + 1);
   } else {
-  printf("%d\t\t%d\t\tNot Allocated\n", i + 1,
- process_size[i]);
-  }
-  }
-  return 0;
-  }
+  printf("%d\t\t%d\t\tNot Allocated\n", i + 1,Enter the size of memory: 1000
+/*Enter the number of memory blocks: 5
+Enter the size of memory block 1: 200
+Enter the size of memory block 2: 300
+Enter the size of memory block 3: 100
+Enter the size of memory block 4: 150
+Enter the size of memory block 5: 250
+Enter the number of processes: 4
+Enter the size of process 1: 120
+Enter the size of process 2: 200
+Enter the size of process 3: 350
+Enter the size of process 4: 70
+
+Process No.     Process Size    Block No.
+1               120             2
+2               200             5
+3               350             Not Allocated
+4               70              1*/
