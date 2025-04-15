d0952ab9f1bd7c582f69d88ab5bc1ac94de73078
Haripriya-23wh1a05g9 <153830065+Haripriya-23wh1a05g9@users.noreply.github.com>
2025-04-15 22:25:46 +0530

    Update bankers_algorithm.c

--- 23WH1A05G9/bankers_algorithm.c
+++ 23WH1A05G9/bankers_algorithm.c
@@ -16,62 +16,67 @@ printf("enter the process ids:");
 
 printf("enter the allocated for each process");
 for(int i=0;i<n;i++){
-for(int j=0;j<res;j++) {
-scanf("%d",&all[i][j]);
-}
+        for(int j=0;j<res;j++) {
+                scanf("%d",&all[i][j]);
+        }
 }
 printf("enter the maximum");
 for(int i=0;i<n;i++){
-for(int j=0;j<res;j++){
-scanf("%d",&max[i][j]);
+        for(int j=0;j<res;j++){
+                scanf("%d",&max[i][j]);
+        }
 }
+for (int i = 0; i < n; i++) {
+        for (int j = 0; j < res; j++) {
+            need[i][j] = max[i][j] - all[i][j];
+        }
+    }
+for(int i=0;i<res;i++){
+        sum[i]=0;
 }
 for(int i=0;i<res;i++){
-sum[i]=0;
+        for(int j=0;j<n;j++){
+                sum[i]=sum[i]+all[j][i];
+        }
 }
 for(int i=0;i<res;i++){
-for(int j=0;j<n;j++){
-sum[i]=sum[i]+all[j][i];
-}
+        avail[i] = tat[i]-sum[i];
 }
 for(int i=0;i<res;i++){
-avail[i] = tat[i]-sum[i];
-}
-for(int i=0;i<res;i++){
-work[i]=avail[i];
+        work[i]=avail[i];
 }
 int finish[n];
 int count=0;
 int safeidx=0;
 for(int i=0;i<n;i++){
-finish[i]=0;
+        finish[i]=0;
 }
 while(count<n){
-int found=0;
-for(int i=0;i<n;i++){
-if(finish[i]==0){
-int exe=1;
-for(int j=0;j<res;j++){
-if(need[i][j]>work[j]){
-exe=0;
-break;
-}
-}
-if(exe==1){
-for(int j=0;j<res;j++){
-work[j]+=all[i][j];
-}
-count++;
-finish[i]=1;
-found=1;
-seq[safeidx++] = p[i];
-break;
-}
-}
+        int found=0;
+        for(int i=0;i<n;i++){
+                if(finish[i]==0){
+                        int exe=1;
+                        for(int j=0;j<res;j++){
+                                if(need[i][j]>work[j]){
+                                        exe=0;
+                                        break;
+                                }
+                        }
+                if(exe==1){
+                        for(int j=0;j<res;j++){
+                                work[j]+=all[i][j];
+                        }
+                        count++;
+                        finish[i]=1;
+                        found=1;
+                        seq[safeidx++] = p[i];
+                        break;
+                }
+        }
 }
 if(found==0){
-printf("System is in unsafe state\n");
-return 1;
+        printf("System is in unsafe state\n");
+        return 1;
 }
 }
 printf("System is in safe state");
