88092e8399bbf33e28a1accb1ea4d5b53734d4e2
Yasasvi2006 <153826647+Yasasvi2006@users.noreply.github.com>
2025-04-27 17:46:32 +0530

    Update sjf.c

--- 23WH1A05G2/sjf.c
+++ 23WH1A05G2/sjf.c
@@ -1,62 +1,65 @@
 #include<stdio.h>
-void bubblesort(int n,int process[],int arrival[],int burst[]);
-void main(){
-	int n;
-	printf("enter the number of processes:");
-	scanf("%d",&n);
 
-	int arrival[n];
-	int process[n];
-	int burst[n];
-	for(int i=0;i<n;i++){
-		process[i]=i+1;
-	}
-	printf("enter arrival times:");
-	for(int i=0;i<n;i++){
-		scanf("%d",&arrival[i]);
-	}
-	printf("enter burst times:");
-	for(int i=0;i<n;i++){
-		scanf("%d",&burst[i]);
-	}
-	bubblesort(n,process,arrival,burst);
-	int ct[n],tt[n],wt[n];
-	int total_time=0;
-	for(int m = 0; m < n; m++) {
-        if(total_time < arrival[m]) {
-            total_time = arrival[m];
+void main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int arrival[n], burst[n], process[n];
+    int ct[n], tt[n], wt[n], completed[n];
+    
+    for(int i = 0; i < n; i++) {
+        process[i] = i + 1;
+        completed[i] = 0; 
+    }
+
+    printf("Enter arrival times: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arrival[i]);
+    }
+
+    printf("Enter burst times: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &burst[i]);
+    }
+
+    int total_time = 0, completed_count = 0;
+
+    while(completed_count < n) {
+        int idx = -1;
+        int min_burst = 1e9;
+
+        for(int i = 0; i < n; i++) {
+            if(arrival[i] <= total_time && completed[i] == 0) {
+                if(burst[i] < min_burst) {
+                    min_burst = burst[i];
+                    idx = i;
+                }
+                else if(burst[i] == min_burst) {
+                    if(arrival[i] < arrival[idx]) {
+                        idx = i;
+                    }
+                }
+            }
         }
-        ct[m] = total_time + burst[m];
-        total_time = ct[m];
+
+        if(idx != -1) {
+            total_time += burst[idx];
+            ct[idx] = total_time;
+            completed[idx] = 1;
+            completed_count++;
+        } else {
+            total_time++;
+        }
     }
 
-   	 for(int l = 0; l < n; l++) {
-        	tt[l] = ct[l] - arrival[l];
-       		 wt[l] = tt[l] - burst[l];
+    for(int i = 0; i < n; i++) {
+        tt[i] = ct[i] - arrival[i];
+        wt[i] = tt[i] - burst[i];
     }
-	 printf("\nprocess\tburst time\twaiting time\tturn-around time\n");
-	 for(int i=0;i<n;i++){
-		 printf("%d\t%d\t%d\t%d\n",process[i],burst[i],wt[i],tt[i]);
-	 }
-}
-void bubblesort(int n, int process[],int arrival[],int burst[]){
-	for(int i=0;i<n-1;i++){
-		for(int j=0;j<n-1;j++){
-			if(burst[j]>burst[j+1]){
-					int temp;
-					
-					temp=burst[j];
-					burst[j]=burst[j+1];
-					burst[j+1]=temp;
 
-					temp=arrival[j];
-					arrival[j]=arrival[j+1];
-					arrival[j+1]=temp;
-
-					temp=process[j];
-					process[j]=process[j+1];
-					process[j+1]=temp;
-			}
-		}
-	}
+    printf("\nProcess\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t\t%d\t\t%d\n", process[i], arrival[i], burst[i], ct[i], tt[i], wt[i]);
+    }
 }
