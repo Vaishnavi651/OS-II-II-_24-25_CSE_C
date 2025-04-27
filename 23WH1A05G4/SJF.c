7b014930260707646c4628d23ff6ed878d5ab0e0
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2025-04-27 13:48:25 +0530

    Update SJF.c

--- 23WH1A05G4/SJF.c
+++ 23WH1A05G4/SJF.c
@@ -8,13 +8,11 @@ int main() {
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-   
     printf("Enter the process IDs: \n");
     for (i = 0; i < n; i++) {
         scanf("%d", &p[i]);
     }
 
-   
     printf("Enter the arrival times of each process: \n");
     for (i = 0; i < n; i++) {
         scanf("%d", &at[i]);
@@ -51,14 +49,12 @@ int main() {
         avg_tat += tat[i];
         avg_wt += wt[i];
     }
-
-  
+    
     printf("\nProcess  AT      BT      CT      TAT     WT");
     for (i = 0; i < n; i++) {
         printf("\nP%-8d%-8d%-8d%-8d%-8d%-8d", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
- 
     printf("\n\nAverage Turnaround Time: %.2f", avg_tat / n);
     printf("\nAverage Waiting Time: %.2f\n", avg_wt / n);
 
