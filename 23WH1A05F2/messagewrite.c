baad4e08725dcfdaf5ad704d7e89ca2ea36fcdf9
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2025-04-28 05:30:34 +0530

    Create messagewrite.c

---
+++ 23WH1A05F2/messagewrite.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MSG_SIZE 100
+
+struct msg_buffer {
+    long msg_type;
+    char msg_text[MSG_SIZE];
+};
+
+int main() {
+    key_t key;
+    int msgid;
+    struct msg_buffer message;
+
+    key = ftok("progfile", 65);
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    if (msgid == -1) {
+        perror("msgget failed");
+        exit(1);
+    }
+
+    message.msg_type = 1;  
+    printf("Enter message: ");
+    fgets(message.msg_text, MSG_SIZE, stdin);
+
+    if (msgsnd(msgid, &message, sizeof(message.msg_text), 0) == -1) {
+        perror("msgsnd failed");
+        exit(1);
+    }
+
+    printf("Message sent: %s\n", message.msg_text);
+    return 0;
+}
